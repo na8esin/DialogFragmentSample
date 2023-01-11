d7a63d7c27322a6b630df5cbd871b038db40f244
Takayuki Watanabe <watanabe@sikmi.com>
2023-01-11 13:40:10 +0900

    add show Dialog method

--- app/src/main/java/com/example/dialogfragmentsample/TopFragment.kt
+++ app/src/main/java/com/example/dialogfragmentsample/TopFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class TopFragment : Fragment(){
@@ -14,7 +15,10 @@ class TopFragment : Fragment(){
     ): View {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_top, container, false)
-
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            SimpleDialogFragment.show(parentFragmentManager)
+        }
         return view
     }
 }
